10ad9bcdc9dd7baa04741805a728752ebba6b685
Adelya <adelya@astar.app>
2022-01-03 17:19:12 -0500

    Add files via upload
    Best case scenario, user input array addition O(n)

---
+++ calculateSum.js
@@ -0,0 +1,10 @@
+//an algorith takes an array of numbers as input and calculates the sum of those numbers
+//define time complexity + determine lowest possible time complexity
+
+function sumNumbers(numbers) {
+    return numbers.reduce((sum, curNum) => sum + curNum ,0);
+}
+
+console.log(sumNumbers([3,10,50]));
+
+//Constant time complexity not possible, only linear, 0(n)
