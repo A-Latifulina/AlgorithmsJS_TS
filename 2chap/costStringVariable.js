bb9c5e7269b971cafd642cde280c578447a64722
Adelya <adelya@astar.app>
2022-01-06 00:06:41 -0500

    Delete costStringVariable.js

--- 2chap/costStringVariable.js
+++
@@ -1,9 +0,0 @@
-var username;
-var message;
-username = Alex;
-message = 'Look!';
-
-var elName = document.getElementById('name');
-elName.textContent = username;
-var elNote = document.getElementById('note');
-elNote.textContent = message;
