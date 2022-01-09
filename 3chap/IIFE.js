fb1641a39618e2bc1980768078fda1f5bf29b8d2
Adelya <adelya@astar.app>
2022-01-09 17:23:43 -0500

    function declaration v function expression v iife

---
+++ 3chap/IIFE.js
@@ -0,0 +1,8 @@
+//function declaration v function expression v immediately invoked function expression
+//immediately invoked function expression (IIFE)
+
+var area = (function() {
+    var w = 3;
+    var h = 2;
+    return w * h;
+}());
---
+++ 3chap/functionDeclaration.js
@@ -0,0 +1,8 @@
+//function declaration v function expression v immediately invoked function expression
+//function declaration
+
+function area(w, h) {
+    return w * h;
+};
+
+var size = area(3, 4);
