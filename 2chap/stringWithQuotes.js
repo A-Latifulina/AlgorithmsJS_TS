266b02a6f657903e9faaa1e0cdbc989aed73fbdc
Adelya <adelya@astar.app>
2022-01-14 00:13:29 -0500

    Delete 2chap directory

--- 2chap/stringWithQuotes.js
+++
@@ -1,9 +0,0 @@
-var title;
-var message;
-title = 'Psssss';
-message = '<a href=\"secret.html\">click for secret!</a>';
-
-var elTitle = document.getElementById('title');
-elTitle.innerHTML = title;
-var elNote = document.getElementById('note');
-elNote.innerHTML = message;
