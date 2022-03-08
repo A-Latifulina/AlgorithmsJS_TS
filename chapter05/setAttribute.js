108b02384d7f599418fb67b5c8e792dd321e2d8c
Adelya <adelya@astar.app>
2022-03-08 08:44:06 -0500

    creating attributes & changing their values

---
+++ chapter05/setAttribute.js
@@ -0,0 +1,7 @@
+//get the first item, change it's class attribute
+var firstItem = document.getElementById('one');
+firstItem.className = 'complete';
+
+//get the fourth item, add an attribute to it
+var fourthItem = document.getElementsByTagName('li').item(3);
+el2.setAttribute('class', 'cool');
